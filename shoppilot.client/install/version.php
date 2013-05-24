5e14dd51f1a11e24cfbad06f9a81111fcf6c646e
Alexander Chaychuk <mr.sashich@gmail.com>
2013-05-24 12:53:05 +0400

    remove previous version from nbs media

--- shoppilot.client/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "1.0.0",
-	"VERSION_DATE" => "2013-04-29 14:50:00"
-);
-?>
--- shoppilot.client/lang/ru/classes/general/shoppilot_api.php
+++
@@ -1,4 +0,0 @@
-<?
-$MESS ['SHOPPILOT_CLIENT_PAYED'] = "Оплачен";
-$MESS ['SHOPPILOT_CLIENT_NO_PAYED'] = "Не оплачен";
-?>
